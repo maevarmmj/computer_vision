301f2de57f5edfd885bd5755c968d909e5e79b85
Mae Mumi <maeva.ramamonjyramboa@reseau.eseo.fr>
2025-04-02 08:18:20 +0200

    comments added

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -5,10 +5,10 @@ class KalmanFilter(object):
         self.dt=dt
         self.pixel_to_meters = pixel_to_meters
 
-        # Vecteur d'etat initial (x, y, vx, vy, ax, ay)
+        # ----------- Vecteur d'etat initial (x, y, vx, vy, ax, ay) ----------------
         self.E=np.matrix([[point[0]], [point[1]], [0], [0], [0], [0]])
 
-        # Matrice de transition
+        # ----------- Matrice de transition ------------------
         self.A=np.matrix([[1, 0, self.dt, 0, 0.5*self.dt**2, 0],
                           [0, 1, 0, self.dt, 0, 0.5*self.dt**2],
                           [0, 0, 1, 0, self.dt, 0],
@@ -35,21 +35,21 @@ class KalmanFilter(object):
 
     def predict(self):
         self.E=np.dot(self.A, self.E)
-        # Calcul de la covariance de l'erreur
+        # ---------- Calcul de la covariance de l'erreur ----------
         self.P=np.dot(np.dot(self.A, self.P), self.A.T)+self.Q
 
-        # -------- Ajout de la gravité --------
-        gravity_pixel_per_frame_sq = 9.81 / self.pixel_to_meters * (self.dt**2) # g en pixels/frame^2
+        # ---------- Ajout de la gravité ----------
+        gravity_pixel_per_frame_sq = 9.81 / self.pixel_to_meters * (self.dt**2)
         self.E[5, 0] = self.E[5, 0] + gravity_pixel_per_frame_sq # On ajoute la gravité à ay
 
         return self.E
 
     def update(self, z):
-        # Calcul du gain de Kalman
+        # -------- Calcul du gain de Kalman --------------
         S=np.dot(self.H, np.dot(self.P, self.H.T))+self.R
         K=np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
 
-        # Correction / innovation
+        # -------- Correction / innovation ---------------
         self.E=np.round(self.E+np.dot(K, (z-np.dot(self.H, self.E))))
         I=np.eye(self.H.shape[1])
         self.P=(I-(K*self.H))*self.P
--- etalonnage.py
+++ etalonnage.py
@@ -4,16 +4,16 @@ import glob
 
 
 def etalonnage():
-    # termination criteria
+    # ---- Termination criteria -----
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    # ---- Préparation des object points, ex : (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0) -----
     objp = np.zeros((7*10, 3), np.float32)
     objp[:, :2] = np.mgrid[0:10, 0:7].T.reshape(-1, 2)
 
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3D points in real world space
-    imgpoints = []  # 2D points in image plane.
+    # ----- Stockage des object points et des image points de toutes les images ---------
+    objpoints = []  # Points 3D points en réel
+    imgpoints = []  # Points 2D sur l'image
 
     images = glob.glob("Checkboard/*.jpg")
 
@@ -21,34 +21,35 @@ def etalonnage():
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-        # Find the chessboard corners
+        # ------- Coins de l'échiquier à trouver ----------
         ret, corners = cv.findChessboardCorners(gray, (10, 7), None)
 
-        # If found, add object points, image points (after refining them)
+        # ------- Si coins trouvés, ajout des object points ----------
         if ret:
             objpoints.append(objp)
             corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners2)
 
-            # Draw and display the corners
+            # -------- Affichage des bords --------------
             cv.drawChessboardCorners(img, (10, 7), corners2, ret)
             cv.imshow('img', img)
             cv.waitKey(500)
 
     cv.destroyAllWindows()
 
-    # Calibration de la caméra
+    # ----------- Calibration de la caméra --------------
     if len(objpoints) > 0 and len(imgpoints) > 0:
         ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-        # Affichage des résultats
+        # --------- Affichage des résultats ------------
         print("\nMatrice intrinsèque de la caméra :\n", mtx)
         print("\nCoefficients de distorsion :\n", dist)
 
-        # Sauvegarde des paramètres
+        # --------- Sauvegarde des paramètres ------------
         np.savez("calibration_data.npz", mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
 
         return mtx, dist
+
     else:
         print("\nErreur : Aucun damier détecté, calibration impossible.")
         return None, None
